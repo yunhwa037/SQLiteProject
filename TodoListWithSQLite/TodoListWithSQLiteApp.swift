14eaa6ecd3284ecc6b9372d1f42418a2efd2cb2f
jang.yunhwa@cloco.co.jp <jang.yunhwa@cloco.co.jp>
2024-12-05 13:53:27 +0900

    Initial Commit

---
+++ TodoListWithSQLite/TodoListWithSQLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListWithSQLiteApp.swift
+//  TodoListWithSQLite
+//
+//  Created by yun on 2024/12/05.
+//
+
+import SwiftUI
+
+@main
+struct TodoListWithSQLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
